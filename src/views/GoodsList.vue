ff104cd95e4aa537570bffb6ccbc9fb812c909c8
clark <clark@yonglang.co>
2019-05-17 17:37:38 +0800

    增加回到顶部功能

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -73,9 +73,31 @@
         </div>
       </modal>
       <div class="md-overlay" v-show="overLayFlag" @click.stop="closePop"></div>
+      <div id="components-back-top-demo-custom">
+        <a-back-top>
+          <div class="ant-back-top-inner">UP</div>
+        </a-back-top>
+        <strong style="color: #1088e9"> blue </strong>
+        button.
+      </div>
       <nav-footer></nav-footer>
     </div>
 </template>
+<style scoped>
+  #components-back-top-demo-custom .ant-back-top {
+    bottom: 100px;
+  }
+  #components-back-top-demo-custom .ant-back-top-inner {
+    height: 40px;
+    width: 40px;
+    line-height: 40px;
+    border-radius: 4px;
+    background-color: #1088e9;
+    color: #fff;
+    text-align: center;
+    font-size: 20px;
+  }
+</style>
 <script>
     import NavHeader from './../components/NavHeader'
     import NavFooter from './../components/NavFooter'
